3ba47b91c38c89c6d34c36542059e2286a0b5749
bernard chisumo <bchisumo74@yahoo.com>
2024-09-18 20:37:18 +0100

    added home name on web route

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
