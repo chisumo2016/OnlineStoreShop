2f488f91eaeeadd5c774a50133e107fe0775929d
bernard chisumo <bchisumo74@yahoo.com>
2024-09-26 13:58:34 +0100

    Vendor Profile Update

---
+++ app/Http/Controllers/Backend/Vendor/ProfileController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Backend\Vendor;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Http\Request;
+use File;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileController extends Controller
+{
+    public function  index()
+    {
+        return view('vendor.dashboard.profile');
+    }
+
+    public function update(ProfileUpdateRequest $request)
+    {
+        /*Grambing  the user */
+        $user = Auth::user();
+
+        /*Handle our image  from request*/
+        if ($request->hasFile('image')){
+            /*Checking  the file exists in database */
+            if (File::exists(public_path($user->image))){
+                /*Delete the old image */
+                File::delete(public_path($user->image));
+            }
+            /**Handle the new Image, store*/
+            $image = $request->image;
+            $imageName = rand().'_'.$image->getClientOriginalName();
+
+            /*Move to storage*/
+            $image->move(public_path('uploads'), $imageName);
+
+            /**Save into database*/
+            $path  = 'uploads/'.$imageName;
+
+            $user->image = $path;
+        }
+
+        /*Assign the  value and save into db **/
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
+
+        toastr()->success('Profile User Updated Successfully');
+        return redirect()->back();
+
+    }
+
+    public function updatePassword(Request $request)
+    {
+        $request->validate([
+            'current_password' => ['required','current_password'],
+            'password' => ['required','confirmed','min:8'],
+        ]);
+
+        // Auth::user();
+        $request->user()->update([
+            'password' => bcrypt($request->password)
+        ]);
+
+        toastr()->success('Vendor Profile Password Updated Successfully');
+        return redirect()->back();
+
+    }
+}
--- routes/vendor.php
+++ routes/vendor.php
@@ -1,6 +1,11 @@
 <?php
+
+use App\Http\Controllers\Backend\Vendor\ProfileController;
 use App\Http\Controllers\Backend\VendorController;
 use Illuminate\Support\Facades\Route;
 
 /** Vendor Routes*/
 Route::get('dashboard', [VendorController::class, 'dashboard'])->name('dashboard');
+Route::get('profile',   [ProfileController::class, 'index'])->name('profile');
+Route::put('profile',   [ProfileController::class,'update'])->name('profile.update');//vendor.profile.update
+Route::post('profile',   [ProfileController::class,'updatePassword'])->name('profile.update.password');
