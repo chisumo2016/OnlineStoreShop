ec66371e1dac9199052f1c7fb2af988296a27d4c
bernard chisumo <bchisumo74@yahoo.com>
2024-11-21 17:13:12 +0000

    Make sidebar  item active  dependiinng on page

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -1,1 +1,13 @@
 <?php
+/**Set Sidebar item active**/
+function setActive(array $route) //pass route name $route
+{
+    if (is_array($route)){
+        foreach ($route as $r){
+            /**current route**/
+            if (request()->routeIs($r)){
+                return 'active';
+            }
+        }
+    }
+}
